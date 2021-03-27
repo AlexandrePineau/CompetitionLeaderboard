4596f7d3f00eeffdcc24b669c1ee15811e05b569
Alexandre Pineau <alecpineau@gmail.com>
2021-03-27 12:09:46 -0400

    updated scoring relation

--- schema.sql
+++ schema.sql
@@ -76,6 +76,7 @@ CREATE TABLE scores (
   event_id int,
   reps int,
   duration_in_seconds int,
+  weight_in_kg int,
   PRIMARY KEY score_id,
   FOREIGN KEY (athlete_id) REFERENCES athletes,
   FOREIGN KEY (event_id) REFERENCES events
